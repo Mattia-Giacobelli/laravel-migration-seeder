5ce01f6fa052549f5fb20369c3088802dff23d59
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-02-13 18:50:40 +0100

    Create the trains table migration

--- database/migrations/2026_02_13_171916_create_trains_table.php
+++ database/migrations/2026_02_13_171916_create_trains_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 75);
+            $table->string('departure_station', 75);
+            $table->string('destination_station', 75);
+            $table->date('departure_day');
+            $table->time('departure_time');
+            $table->string('arrival_time');
+            $table->string('train_code', 100);
+            $table->smallInteger('carriages');
+            $table->boolean('on_time');
+            $table->boolean('canceled');
             $table->timestamps();
         });
     }
