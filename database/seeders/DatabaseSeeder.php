87ee421f3c51ab691bbf7de963153d529459a87e
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-02-14 18:24:30 +0100

    Create fake data with Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $this->call(StudentsTableSeeder::class);
+        $this->call(TrainsTableSeeder::class);
     }
 }
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,10 +6,31 @@ use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+//Import Faker
+use Faker\Generator as Faker;
+
 class TrainsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void {}
+
+    public function run(Faker $faker): void
+    {
+
+        for ($i = 0; $i < 200; $i++) {
+
+            $newTrain = new Train();
+
+            $newTrain->agency = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->destination_station = $faker->city();
+            $newTrain->departure_day = $faker->date('Y-m-d', '2026-12-31');
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->ean8();
+            $newTrain->carriages = $faker->numberBetween(5, 20);
+            $newTrain->on_time = $faker->boolean(0.8);
+            $newTrain->canceled = $faker->boolean(0.1);
+
+            $newTrain->save();
+        }
+    }
 }
